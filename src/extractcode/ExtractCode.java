a3ab6447d4ff8fa78f5c2e7c7f595f8b7960f696
wangtx13 <zjwangtx13@gmail.com>
2015-05-08 22:59:00 +0800

    omit package and import

--- src/extractcode/ExtractCode.java
+++ src/extractcode/ExtractCode.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package extractcode;
 
 import java.io.BufferedReader;
@@ -24,72 +23,72 @@ import java.util.logging.Logger;
  */
 public class ExtractCode {
 
-    
     public static void extractCode(File inputFile, File outputFile) {
-        
+
         boolean commentsStart = false;
         StringBuffer outputStr = new StringBuffer();
         StringBuffer lastOutputStr = new StringBuffer();
-        
-        if(!inputFile.exists()) {
+
+        if (!inputFile.exists()) {
             System.out.println("File doesn't exist!");
-        } else if(inputFile.isDirectory()) {
+        } else if (inputFile.isDirectory()) {
             System.out.println(inputFile.getName() + " is a directory. Please input a file.");
         } else {
             try {
                 //get input file name
                 String inputFilePath = inputFile.getPath();
-                
-                try(
-                    InputStream in = new FileInputStream(inputFilePath);
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                    BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))
-                ){
+
+                try (
+                        InputStream in = new FileInputStream(inputFilePath);
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
                     //read lines from the file
                     String lineString = null;
-                    while((lineString = reader.readLine()) != null) {
-                        int strLength = lineString.length();
-                        char currentChar = '$';
-                        char lastChar = '$';
-                        char lastLastChar = '$';
-                        for(int i = 0; i < strLength; ++i) {
-                            currentChar = lineString.charAt(i);
-                            
-                            if(currentChar == '*' && lastChar == '/' && !commentsStart) {
-                                outputStr.deleteCharAt(outputStr.length() - 1);
-                                commentsStart = true;
-                            } else if(currentChar == '/' && lastChar == '*' && lastLastChar != '/' && commentsStart) {
-                                commentsStart = false;
-                            } else if(currentChar == '/' && lastChar == '/' && lastLastChar != '*' && !commentsStart) {
-                                outputStr.deleteCharAt(outputStr.length() - 1);
-                                i = strLength;
-                            } else if(!commentsStart) {
-                                outputStr.append(currentChar);
+                    while ((lineString = reader.readLine()) != null) {
+                        if (!lineString.contains("import") && !lineString.contains("package")) {
+                            int strLength = lineString.length();
+                            char currentChar = '$';
+                            char lastChar = '$';
+                            char lastLastChar = '$';
+                            for (int i = 0; i < strLength; ++i) {
+                                currentChar = lineString.charAt(i);
+
+                                if (currentChar == '*' && lastChar == '/' && !commentsStart) {
+                                    outputStr.deleteCharAt(outputStr.length() - 1);
+                                    commentsStart = true;
+                                } else if (currentChar == '/' && lastChar == '*' && lastLastChar != '/' && commentsStart) {
+                                    commentsStart = false;
+                                } else if (currentChar == '/' && lastChar == '/' && lastLastChar != '*' && !commentsStart) {
+                                    outputStr.deleteCharAt(outputStr.length() - 1);
+                                    i = strLength;
+                                } else if (!commentsStart) {
+                                    outputStr.append(currentChar);
+                                }
+
+                                lastLastChar = lastChar;
+                                lastChar = currentChar;
                             }
-                             
-                            lastLastChar = lastChar;
-                            lastChar = currentChar;
+
+                            //Whether the line includes comments
+                            if (!outputStr.toString().equals(lastOutputStr.toString())) {
+                                outputStr.append("\r\n");
+                            }
+                            lastOutputStr.replace(0, outputStr.length(), outputStr.toString());
                         }
-                        
-                        //Whether the line includes comments
-                        if(!outputStr.toString().equals(lastOutputStr.toString())) {
-                            outputStr.append("\r\n");
-                        }
-                        lastOutputStr.replace(0, outputStr.length(), outputStr.toString());
                     }
-                    
+
                     outputStr = ParseWords.parseAllWords(outputStr);
                     writer.write(outputStr.toString());
                     writer.flush();
-                } 
-                
+                }
+
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(ExtractCode.class.getName()).log(Level.SEVERE, null, ex);
             } catch (IOException ex) {
                 Logger.getLogger(ExtractCode.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
+
     }
-    
+
 }
